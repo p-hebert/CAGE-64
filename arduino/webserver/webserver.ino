f814c0d3613b33edada2ead05e9ef26ba8eb1a9f
Alex Gagné <alex_gagne94@hotmail.com>
2016-10-22 16:48:39 -0400

    using cstrings instead of arduino strings,  should be faster

--- arduino/webserver/webserver.ino
+++ arduino/webserver/webserver.ino
@@ -8,6 +8,87 @@ const byte pin = 12;
 
 YunServer server;
 
+/*
+ * Bytes to signal out.
+ * First and second bytes are to identify the controller and are static.
+ * Last is:
+ *  - 0x01 if there is a controller pack,
+ *  - 0x02 if not,
+ *  - 0x04 if the previous controller read/write address CRC showed an error.
+*/
+const byte outBytes[] = { B00000101,B00000000,B00000000,B00000000,B00000000,B00000000,B00000010 };
+byte out[] = { B00000101,B00000000,B00000000,B00000000,B00000000,B00000000,B00000010 };
+
+void processInput(char* input){
+
+  switch(input[0]){
+    case 'A':    
+      out[3] = out[3] | B10000000;
+      break;
+    case 'B':
+      out[3] = out[3] | B01000000;
+      break;
+    case 'S':
+      out[3] = out[3] | B00100000;
+      break;
+    case 'Z':
+      out[3] = out[3] | B00010000;
+      break;
+    case 'D':
+      switch(input[1]){
+        case 'u':
+          out[3] = out[3] | B00001000;
+          break;
+        case 'd':
+          out[3] = out[3] | B00000100;
+          break;
+        case 'l':
+          out[3] = out[3] | B00000010;
+          break;
+        case 'r':
+          out[3] = out[3] | B00000001;
+          break;
+      }
+      break;
+    case 'L':
+      out[4] = out[4] | B00100000;
+      break;
+    case 'r':
+      out[4] = out[4] | B00010000;
+      break;
+    case 'C':
+      switch(input[1]){
+        case 'u':
+          out[4] = out[4] | B00001000;
+          break;
+        case 'd':
+          out[4] = out[4] | B00000100;
+          break;
+        case 'l':
+          out[4] = out[4] | B00000010;
+          break;
+        case 'r':
+          out[4] = out[4] | B00000001;
+          break;
+      }
+      break;
+    case 'J':
+      char value[3] = {input[2], input[3], input[4]};
+        int val = atoi(value);
+        if(val > 0 && val < 101){
+          switch(input[1]){
+          case 'x':
+            out[5] = (byte)(val-50);
+            break;
+          case 'y':
+            out[6] = (byte)(val-50);
+            break;
+          }
+        }
+      break;
+  }
+}
+
 void setup() {
   pinMode(internalLED, OUTPUT);
   pinMode(pin, INPUT);
@@ -19,99 +100,20 @@ void setup() {
 
 void loop() {
   YunClient client = server.accept();  // Get clients coming from server
+  
   if (client)                          // Is there a new client?
   {
     String str = client.readString();
-    /*
-     * Bytes to signal out.
-     * First and second bytes are to identify the controller and are static.
-     * Last is:
-     *  - 0x01 if there is a controller pack,
-     *  - 0x02 if not,
-     *  - 0x04 if the previous controller read/write address CRC showed an error.
-    */
-    byte out[] = { B00000101,B00000000,B00000000,B00000000,B00000000,B00000000,B00000010 };
 
-    //Encoding the third byte
-    //A
-    if(str.indexOf("A") != -1){
-      out[3] = out[3] | B10000000;
-    }
-    //B
-    if(str.indexOf("B") != -1){
-      out[3] = out[3] | B01000000;
-    }
-    //Start
-    if(str.indexOf("S") != -1){
-      out[3] = out[3] | B00100000;
-    }
-    //Z
-    if(str.indexOf("Z") != -1){
-      out[3] = out[3] | B00010000;
-    }
-    //D-pad
-    if(str.indexOf("Du") != -1){
-      out[3] = out[3] | B00001000;
-    }
-    if(str.indexOf("Dd") != -1){
-      out[3] = out[3] | B00000100;
-    }
-    if(str.indexOf("Dl") != -1){
-      out[3] = out[3] | B00000010;
-    }
-    if(str.indexOf("Dr") != -1){
-      out[3] = out[3] | B00000001;
-    }
+    char* cstr = strdup(str.c_str());
 
-    //Encoding the fourth byte
-    //Joy Reset : do nothing
-    //Second most significant bit: do nothing
-    //L
-    if(str.indexOf("L") != -1){
-      out[4] = out[4] | B00100000;
+    char* input = strtok (cstr,":");
+    while (input != NULL)
+    {
+      processInput(input);
+      input = strtok (NULL, ":");
     }
-    //R
-    if(str.indexOf("R") != -1){
-      out[4] = out[4] | B00010000;
-    }
-    //C-Pad
-    if(str.indexOf("Cu") != -1){
-      out[4] = out[4] | B00001000;
-    }
-    if(str.indexOf("Cd") != -1){
-      out[4] = out[4] | B00000100;
-    }
-    if(str.indexOf("Cl") != -1){
-      out[4] = out[4] | B00000010;
-    }
-    if(str.indexOf("Cr") != -1){
-      out[4] = out[4] | B00000001;
-    }
-    //Encoding the fifth byte
-    //Joystick-X
-    int index, val;
-    bool isNumber = true;
-    String str_val;
-    if(str.indexOf("Jx") != -1){
-      index = str.indexOf("Jx");
-      if(str.length() > index + 3 && isInt(str.substring(index+2, index+6))){
-        val = str.substring(index+2, index+6).toInt();
-        if(val > 0 && val < 101){
-          out[5] = (byte)(val-50);
-        }
-      }
-    }
-    //Encoding the sixth byte
-    //Joystick-Y
-    if(str.indexOf("Jy") != -1){
-      index = str.indexOf("Jy");
-      if(str.length() > index + 3 && isInt(str.substring(index+2, index+6))){
-        val = str.substring(index+2, index+6).toInt();
-        if(val > 0 && val < 101){
-          out[6] = (byte)(val-50);
-        }
-      }
-    }
+        
     //Signal the bytes out!
     if(test){
       for(int i = 0 ; i < sizeof(out)/sizeof(byte) ; i++){
